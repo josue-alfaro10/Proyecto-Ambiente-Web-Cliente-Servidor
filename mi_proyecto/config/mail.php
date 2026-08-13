0033edd0e31f0e618067f1df249ab9748d3137de
josue alfaro <josuealfaroc28@gmail.com>
2026-08-13 14:40:26 -0600

    fix

---
+++ mi_proyecto/config/mail.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => 587,
+    'smtp_user' => 'pawfinder0@gmail.com',
+    'smtp_pass' => 'ligiqwazczpajihe',
+    'smtp_from_name' => 'PawFinder',
+];
